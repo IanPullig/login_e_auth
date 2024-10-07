9144519adb8bf4d1ded50b1bcdab89aa773f5f06
Ianps-Pull <ianpullig1@gmail.com>
2024-10-07 00:38:36 -0400

    Add files via upload

---
+++ auth-quarkus/src/main/java/org/acme/entity/UserEntity.java
@@ -0,0 +1,23 @@
+package org.acme.entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.*;
+
+import javax.annotation.processing.Generated;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_users")
+
+public class UserEntity extends PanacheEntityBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    public UUID userId;
+
+    public String username;
+
+
+
+
+}
